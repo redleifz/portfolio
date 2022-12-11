078c9874da9852fb784e8f34c95b5890962b9e28
Jongjate Choomjairug <jongjate.ch@gmail.com>
2022-12-11 23:18:15 +0700

    addMainPicture

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillPersonLinesFill, BsFillPersonsFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Image from "next/image";
+import MainImg from "../public/assets/main.jpeg"
 
 const Main = () => {
   return (
@@ -10,22 +12,31 @@ const Main = () => {
       id="home"
       className="flex items-center justify-center h-screen mb-3 bg-fixed bg-center bg-cover  z-[-2] "
     >
-      {/* <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" /> */}
+      <div className="absolute h-screen top-0 left-0 right-0 bottom-0 bg-black/70 z-[1]" />
+       <div className=" absolute h-scren top-0 left-0 right-0 bottom-0">
+        <Image
+          src={MainImg}
+          fill
+          alt="/"
+        />
+       </div>
+    
+     
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div className="z-[2] text-center">
-          <p className="uppercase py-4 text-sm tracking-wide text-[#EEEEEE]">
-              Let&apos;s build something together
+          <p className="uppercase py-4 xs:mt-3 text-sm tracking-wide text-[#EEEEEE]">
+            Let&apos;s build something together
           </p>
           <h1 className="py-4 text-[#EEEEEE]">
-          Hi, I&apos;m <span className="text-[#FF8C32]">Jongjate</span>
+            Hi, I&apos;m <span className="text-[#FF8C32]">Jongjate</span>
           </h1>
           <h1 className="py-4 text-[#EEEEEE]">A Front-End Web Developer</h1>
           <p>
             I am an expert in website front-end development. (And there are
             times when I do my own design as I have some experience with
             graphics programs.) I am currently focusing on front-end web
-            development. However, I&apos;m also interested in learning about back-end
-            development.
+            development. However, I&apos;m also interested in learning about
+            back-end development.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-10 mt-10">
             <Link href="https://www.linkedin.com/in/jongjate-choomjairug-352a79211/">
