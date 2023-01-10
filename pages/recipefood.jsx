77a2a0b3e4f9697797142da07121c8838a8a1087
Jongjate <83629650+redleifz@users.noreply.github.com>
2023-01-10 17:33:05 +0700

    Update recipefood.jsx

--- pages/recipefood.jsx
+++ pages/recipefood.jsx
@@ -18,7 +18,7 @@ const recipefood = () => {
         />
 
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Find Your Trip</h2>
+          <h2 className="py-2">Rectipe Food</h2>
           <h3>ReactJs/Tailwind</h3>
         </div>
       </div>
