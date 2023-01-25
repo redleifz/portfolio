8751f78f6aeb7e9b2e2298155fd011733bf7256a
Jongjate <83629650+redleifz@users.noreply.github.com>
2023-01-25 09:25:05 +0700

    add key prop

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,15 +1,11 @@
 import Link from "next/link";
 import React from "react";
-import {GrReactjs} from 'react-icons/gr'
-import {SiTailwindcss} from 'react-icons/si'
-import {SiMongodb} from 'react-icons/si'
-import {SiExpress} from 'react-icons/si'
-import {GrNode} from 'react-icons/gr'
-import {SiFirebase} from 'react-icons/si'
-
-
-
-
+import { GrReactjs } from "react-icons/gr";
+import { SiTailwindcss } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { GrNode } from "react-icons/gr";
+import { SiFirebase } from "react-icons/si";
 
 const ProjectCard = ({ image, title, detail, techstack, demoUrl, codeUrl }) => {
   return (
@@ -24,22 +20,39 @@ const ProjectCard = ({ image, title, detail, techstack, demoUrl, codeUrl }) => {
         </div>
         <div className="h-[8%] flex flex-row items-center">
           <div>
-            <span className="p-2 font-bold text-white text-[15px]">Tech Stack : </span>
+            <span className="p-2 font-bold text-white text-[15px]">
+              Tech Stack :{" "}
+            </span>
           </div>
           <div className="flex flex-row">
             {techstack.map((tech) => {
               if (tech === "React") {
-                return <GrReactjs className="text-blue-500 mr-[3px]" />;
+                return (
+                  <GrReactjs key={tech} className="text-blue-500 mr-[3px]" />
+                );
               } else if (tech === "Tailwind") {
-                return <SiTailwindcss className="text-blue-300 mr-[3px]" />;
+                return (
+                  <SiTailwindcss
+                    key={tech}
+                    className="text-blue-300 mr-[3px]"
+                  />
+                );
               } else if (tech === "MongoDB") {
-                return <SiMongodb className="text-green-500 mr-[3px]" />;
+                return (
+                  <SiMongodb key={tech} className="text-green-500 mr-[3px]" />
+                );
               } else if (tech === "Express") {
-                return <SiExpress className="text-slate-200 mr-[3px]" />;
-              }else if (tech === "NodeJs") {
-                return <GrNode className="text-yellow-200 mr-[3px]" />;
-              }else if (tech === "Firebase") {
-                return <SiFirebase className="text-orange-400 mr-[3px]" />;
+                return (
+                  <SiExpress key={tech} className="text-slate-200 mr-[3px]" />
+                );
+              } else if (tech === "NodeJs") {
+                return (
+                  <GrNode key={tech} className="text-yellow-200 mr-[3px]" />
+                );
+              } else if (tech === "Firebase") {
+                return (
+                  <SiFirebase key={tech} className="text-orange-400 mr-[3px]" />
+                );
               }
             })}
           </div>
